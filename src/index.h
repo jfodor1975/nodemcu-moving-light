b295cd76175bdaff9b70bf88aeff9d80fdd69ba5
jfodor1975 <jeff.fodor@gmail.com>
2021-02-21 14:38:31 -0500

    webpage modified

--- src/index.h
+++ src/index.h
@@ -3,11 +3,44 @@ const char MAIN_page[] PROGMEM = R"=====(
 <html>
 <body>
 <center>
-<h1>WiFi LED on off demo: 1</h1><br>
+<h1>WiFi Desk Light</h1><br>
 Ciclk to turn <a href="Testmode">Led Test</a><br>
-Ciclk to turn <a href="Fullreset">Reset</a><br>
+Ciclk to turn <a href="PanTiltreset">Pan/Tilt Reset</a><br>
 <hr>
 <a href="https://circuits4you.com">circuits4you.com</a>
+</center>
+
+</body>
+</html>
+)=====";
+
+const char PanTilt_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>WiFi Desk Light</h1><br>
+Reseting the Pan and Tilt<br>
+this will move thru a full range of motion on the Pan and the tilt axis<br>
+<hr>
+<input type="button" value="Go Back" onclick="history.back(-1)" />
+</center>
+
+</body>
+</html>
+)=====";
+
+const char Ledreset_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>WiFi Desk Light</h1><br>
+Reseting the LED's<br>
+You should see an ramp of RGB thru all 7 leds<br>
+SACN will resme control when complete<br>
+<hr>
+<input type="button" value="Go Back" onclick="history.back(-1)" />
 </center>
 
 </body>

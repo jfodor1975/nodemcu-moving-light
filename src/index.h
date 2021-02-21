339e10ffff0f5faf59ac811415c13a3b340aad1f
jfodor1975 <jeff.fodor@gmail.com>
2021-02-21 14:10:55 -0500

    init index.h file

---
+++ src/index.h
@@ -0,0 +1,15 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h1>WiFi LED on off demo: 1</h1><br>
+Ciclk to turn <a href="Testmode">Led Test</a><br>
+Ciclk to turn <a href="Fullreset">Reset</a><br>
+<hr>
+<a href="https://circuits4you.com">circuits4you.com</a>
+</center>
+
+</body>
+</html>
+)=====";

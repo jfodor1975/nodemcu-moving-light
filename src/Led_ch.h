55a601314f159c20f9ffe66732d1b9662f514bef
jfodor1975 <jeff.fodor@gmail.com>
2021-03-04 00:55:14 -0500

    chagning e131 libarys

--- src/Led_ch.h
+++ src/Led_ch.h
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-//Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
-
+// neo pixel libary code for updateing the led's
 void set_led_data()
 {
-int ch = 4;
+  if (e131.universe == Uni) {  
+    //int ch = 4;
         
-        for (int f = 0; f < NUM_LEDS; f++)
+        for (int f = 0; f < NUM_LEDS; f++){
+            int ch = f * 3 + 4;
             //pixels.setPixelColor(f, pixels.Color(0, 255, 0));
-            pixels.setPixelColor(f, pixels.Color(e131.data[ch], e131.data[ch + 1], e131.data[ch + 2])), 
-            ch = ch + 3;
-
+            //pixels.setPixelColor(f, pixels.Color(e131.data[ch], e131.data[ch + 1], e131.data[ch + 2])),
+            pixels.setPixelColor(f, e131.data[ch], e131.data[ch + 1], e131.data[ch + 2]);
+    //        ch = ch + 3;
+        }
         pixels.show();
-
+        //delay(20);
+  }
 
 }
